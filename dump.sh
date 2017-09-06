18057ace0e7278396ad52a3ae75bdc51f81e9330
Jimmie <jimmiebtlr@gmail.com>
2017-09-06 11:12:09 +0200

    Do something with quotes and commas inside fields of csv.

--- dump.sh
+++ dump.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-for tn in `mysql --batch --skip-page --skip-column-name --raw -h35.202.122.232 -uroot -pVAriv6tK -e"show tables from $DB_NAME"`
+for tn in `mysql --batch --skip-page --skip-column-name --raw -h$DB_IP -uroot -p$DB_PASSWORD -e"show tables from $DB_NAME"`
 do 
-  mysql -uroot -p$PASSWORD -h$DB_IP $DB_NAME -B -e "select * from \`$tn\`;" | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' > tables/$tn.csv
+  file=tables/$tn.csv
+  if [ -f $file ]; then
+    echo "Skipping $tn"
+  else 
+    # Replace quotes with "" in accordance with RFC-4180
+    # idk what most of others are, find more at bottom
+    echo "Exporting $tn"
+    mysql -uroot -p$DB_PASSWORD -h$DB_IP $DB_NAME -B -e "select * from \`$tn\`;" | pv -lq | sed 's/"/U+0022/g;s/,/U+060C/g;s/\t/","/g;s/^/"/;s/$/"/;s/\n//g' > $file &
+    sleep 10
+  fi
 done
+
+# Some resources
+# https://stackoverflow.com/questions/17808511/properly-escape-a-double-quote-in-csv
+# https://stackoverflow.com/questions/356578/how-to-output-mysql-query-results-in-csv-format
--- load.sh
+++ load.sh
@@ -11,7 +11,7 @@ process_table() {
 
     # Load head, seems to fix autodetect issue for larger files
     echo "${table} - Loading head version"
-    bq load --skip_leading_rows 1 --null_marker NULL --allow_quoted_newlines --autodetect --schema_update_option ALLOW_FIELD_ADDITION --allow_quoted_newlines phrg.${table} heads/${table}.csv
+    bq load --skip_leading_rows 1 --null_marker NULL --allow_quoted_newlines --autodetect --schema_update_option ALLOW_FIELD_ADDITION phrg.${table} heads/${table}.csv
 
     echo "${table} - Saving schema"
     bq show --schema phrg.${table} > schemas/${table}.json
@@ -22,11 +22,11 @@ process_table() {
     # Change this after I'm done.  Uploaded in a separate step, so can skip that and go directly from local in future
     # I'm referring to the gs:// designation, you can point to local fs instead
     echo "${table} - Loading full table"
-    bq load --skip_leading_rows 1 --null_marker NULL --allow_quoted_newlines phrg.${table} gs://phrg-datatables/${table}.csv schemas/${table}.json
+    bq load --skip_leading_rows 1 --null_marker NULL --allow_quoted_newlines phrg.${table} tables/${table}.csv schemas/${table}.json
   else
     if [[ $(wc -l < tables/${table}.csv) -ge 2 ]]; then
       echo "${table} - Loading full table"
-      bq load --skip_leading_rows 1 --null_marker NULL --autodetect --schema_update_option ALLOW_FIELD_ADDITION --allow_quoted_newlines phrg.${table} gs://phrg-datatables/${table}.csv
+      bq load --skip_leading_rows 1 --null_marker NULL --autodetect --schema_update_option ALLOW_FIELD_ADDITION --allow_quoted_newlines phrg.${table} tables/${table}.csv
     else 
       echo "${table} - Skipping, empty"
     fi
@@ -40,5 +40,5 @@ do
   # Slow it down a bit, to many open wouldn't be great
   # not certain how to properly implement a max number of uploads 
   # in bash without some complication
-  sleep 2
+  sleep 5
 done
